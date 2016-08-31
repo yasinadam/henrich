a25c99d086ecdcc334604fa3e60606d03408a965
dev <yasinadam@outlook.com>
2016-08-31 23:47:59 +0100

    heroku

--- config/database.js
+++ config/database.js
@@ -1,3 +1,4 @@
 module.exports = {
-    'url' : 'mongodb://127.0.0.1/henrich'
+    //'url' : 'mongodb://127.0.0.1/henrich'
+    'url' : 'mongodb://heroku_29640z1j:henrich557788@ds019986.mlab.com:19986/heroku_29640z1j'
 };
